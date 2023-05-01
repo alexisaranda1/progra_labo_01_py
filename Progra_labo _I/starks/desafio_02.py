ae7f18eca511c0538b6bd39da77dbe5bc43ae086
alexisaranda1 <arandajaaraalexis@gmail.com>
2023-05-01 18:58:42 -0300

    stark 02

--- Progra_labo _I/starks/desafio_02.py
+++ Progra_labo _I/starks/desafio_02.py
@@ -1,7 +1,7 @@
-import os
+
 from data_stark import lista_personajes
 
-
+import os
 def clear_console() -> None:
     """
     It waits for the user to hit enter 
@@ -10,8 +10,13 @@ def clear_console() -> None:
     _ = input('Press a key to continue...')
     os.system('cls')
 
-
+#0
 def stark_normalizar_datos(heroes: list[dict]) -> None:
+    '''Recorrer la lista y convertir al tipo de dato correcto las keys (solo con las keys que representan datos numéricos)
+    Validar primero que el tipo de dato no sea del tipo al cual será casteado. Por ejemplo si una key debería ser entero (ejemplo edad) verificar antes que no se encuentre ya en ese tipo de dato.
+    Si al menos un dato fue modificado, la función deberá imprimir como mensaje ‘Datos normalizados’, caso contrario no imprimirá nada.
+    Validar que la lista de héroes no esté vacía para realizar sus acciones, caso contrario imprimirá el mensaje: “Error: Lista de héroes vacía”
+    '''
 
     if heroes:
         # Recorro cada diccionario de heroe de la lista
@@ -30,11 +35,11 @@ def stark_normalizar_datos(heroes: list[dict]) -> None:
                             heroe[key] = float(heroe[key])
                         else:
                             heroe[key] = int(heroe[key])
-                            print(f'El dato {key} fue modificada para el heroe: {heroe["nombre"]}')
+                            #print(f'El dato {key} fue modificada para el heroe: {heroe["nombre"]}')
     else:
         print('Error La lista esta vacía.')
 
-
+#1.1
 def obtener_nombre(heroe):
     '''Crear la función 'obtener_nombre' la cual recibirá por parámetro
     un diccionario el cual representara a un héroe y devolverá un string 
@@ -48,7 +53,7 @@ def obtener_nombre(heroe):
     else:
         return "Nombre no encontrado"
 
-
+#1.2
 def imprimir_dato(cadena_caracteres: str):
     '''Crear la función 'imprimir_dato' la cual recibirá por parámetro un string
     y deberá imprimirlo en la consola.
@@ -59,7 +64,7 @@ def imprimir_dato(cadena_caracteres: str):
     else:
         print("No es una cadena de texto")
 
-
+#1.3
 def stark_imprimir_nombres_heroes(lista_heroes: list):
     '''Crear la función 'stark_imprimir_nombres_heroes'
     la cual recibirá por parámetro la lista de héroes y 
@@ -74,7 +79,7 @@ def stark_imprimir_nombres_heroes(lista_heroes: list):
         nombre = obtener_nombre(heroe)
         imprimir_dato(nombre)
 
-
+#1.4
 def obtener_dato(heroe: dict, key):
     dato = heroe[key]
     if dato:
@@ -82,10 +87,11 @@ def obtener_dato(heroe: dict, key):
     else:
         print("No es un dato valido")
 
-
+#2
 def obtener_nombre_y_dato(heroe: dict, key: str) -> str:
     ''' Crear la función 'obtener_nombre_y_dato' la misma recibirá por 
-    parámetro un diccionario el cual representara a un héroe y una key (string) la cual representará el dato que se desea obtener. 
+    parámetro un diccionario el cual representara a un héroe y una key (string)
+    la cual representará el dato que se desea obtener. 
 
     La función deberá devolver un string el cual contenga 
     el nombre y dato (key) del héroe a imprimir. El dato puede ser 
@@ -104,6 +110,7 @@ def obtener_nombre_y_dato(heroe: dict, key: str) -> str:
     else:
         return '{} | Dato no encontrado'.format(nombre)
 
+#3
 
 def stark_imprimir_nombres_alturas(lista_heroes: list):
     """Crear la función 'stark_imprimir_nombres_alturas' 
@@ -115,9 +122,11 @@ def stark_imprimir_nombres_alturas(lista_heroes: list):
     if lista_heroes:
         for heroe in lista_heroes:
             nombre_altura_obtenido = obtener_nombre_y_dato(heroe, "altura")
-            print(nombre_altura_obtenido)
+            imprimir_dato(nombre_altura_obtenido)
     else:
         return -1
+
+#4.1
 
 
 def calcular_max(lista, key):
@@ -132,7 +141,11 @@ def calcular_max(lista, key):
         if lista[i][key] > max_valor:
             max_valor = lista[i][key]
             max_heroe = lista[i]
+            
     return max_heroe
+
+
+#4.2
 
 
 def calcular_min(lista, key):
@@ -140,7 +153,7 @@ def calcular_min(lista, key):
     min_heroe = lista[0]
 
     for i in range(len(lista)):
-        # for heroe in lista[1:]:
+     
         if lista[i][key] < min_valor:
             min_valor = lista[i][key]
             min_heroe = lista[i]
@@ -148,26 +161,197 @@ def calcular_min(lista, key):
     return min_heroe
 
 
+#4.3
+
+
 def calcular_max_min_dato(lista_heroes, tipo_calculo, key_dato):
+    
     if tipo_calculo == "maximo" and key_dato in lista_heroes[0]:
         maximo = calcular_max(lista_heroes, key_dato)
         return maximo
     elif tipo_calculo == "minimo" and key_dato in lista_heroes[0]:
 
         minimo = calcular_min(lista_heroes, key_dato)
-        print(minimo["altura"])
+       # print(minimo["altura"])
         return minimo 
     else:
         print("todo mal!")
+        
+            
+#4.4
+
+def stark_calcular_imprimir_heroe (lista_heroes: list , max_min : str, key : str) :
+    '''La lista de héroes
+El tipo de cálculo a realizar: es un valor string que puede tomar los valores ‘maximo’ o ‘minimo’
+Un string que representa la key del dato a calcular, por ejemplo: ‘altura’, ‘peso’, ‘edad’, etc.'''
+    if lista_heroes:
+        
+       heore_obtenido =  calcular_max_min_dato(lista_heroes, max_min, key)
+       heore_obtenido = obtener_nombre_y_dato(heore_obtenido,key)
+       imprimir_dato(heore_obtenido)
+       
+    else:
+        return -1
+    
+    
+#5.1
+def sumar_dato_heroe(lista_heroes, key):
+    
+    '''Crear la función 'sumar_dato_heroe' la cual recibirá como parámetro una lista de 
+    héroes y un string que representara el dato/key de los héroes que se requiere sumar. 
+    Validar que cada héroe sea tipo diccionario y que no sea un diccionario vacío antes 
+    de hacer la suma. La función deberá retorna la suma de todos los datos según la key
+    pasada por parámetro'''
+    
+    suma = 0
+    
+    for heroe in lista_heroes:
+        
+        if type(heroe) is dict and len(heroe) > 0 :
+            
+            if key == "altura" or key == "peso" or key == "fuerza" :
+            
+                suma += heroe[key]
+            else:
+                print("Dato key!!")   
+        else :
+            
+            return 0
+        
+    return suma
+#5.2
+def dividir(dividendo, divisor):
+    
+    '''Crear la función  ‘dividir’ la cual recibirá como parámetro dos números 
+    (dividendo y divisor). Se debe verificar si el divisor es 0,  en caso de serlo, 
+    retornar 0, caso contrario realizar la división entre los parámetros y retornar
+    el resultado'''
+    
+    print("numero dividendo: {0}".format(dividendo))
+    if divisor == 0:
+        return 0
+    else:
+        resultado = dividendo / divisor
+        
+    return resultado
+
+
+#5.3
+def calcular_promedio(lista_heroes, dato_calcular):
+    '''crear la función ‘calcular_promedio’ la cual recibirá como parámetro una lista de héroes
+    y un string que representa el dato del héroe que se requiere calcular el promedio.
+    La función debe retornar el promedio del dato pasado por parámetro'''
+    
+    suma = 0
+    cantidad = len(lista_heroes)
+    
+    if cantidad > 0:
+        suma = sumar_dato_heroe(lista_heroes, dato_calcular)
+        promedio = dividir(suma, cantidad )
+    else :
+        
+        return 0
+            
+    promedio = suma / cantidad
+    
+    return promedio
+
+
+#5.4
+def stark_calcular_imprimir_promedio_altura(lista_heroes):
+    '''Crear la función 'stark_calcular_imprimir_promedio_altura' 
+la cual recibirá una lista de héroes y utilizando la función del punto 5.3
+calcula y mostrará la altura promedio. Validar que la lista de héroes no 
+esté vacía para realizar sus acciones, caso contrario no hará nada y retornara -1.'''
+
+    if lista_heroes :
+        
+        promedio_altura = calcular_promedio(lista_heroes,"altura")
+
+        # promedio_altura_string = "Promedio altura es : {0}".format(promedio_altura)
+        # imprimir_dato(promedio_altura_string)
+  
+        print("Promedio altura es : {0}".format(promedio_altura))
+        
+    else:
+        return -1
+    
+    
+#6.1
+def imprimir_menu():
+    
+    '''Crear la función "imprimir_menu" que imprima el menú de opciones por pantalla, 
+    el cual permite utilizar toda la funcionalidad ya programada. 
+    Se deberá reutilizar la función antes creada encargada de imprimir un string (1.2)
+    '''
+    imprimir_dato('\t1) Imprimir nombres :\n\t2) Imprimir Nombre y Altura :'
+                  '\n\t3) cacular maximo Fuerza  \n\t 4) Calular promedio Altura:')
+
+
+#6.2
+
+def validar_entero(cadena_numeros: str) -> bool:
+    '''Crear la función “validar_entero” la cual recibirá 
+por parámetro un string de número el cual deberá verificar
+que sea sea un string conformado únicamente por dígitos. 
+Retornara True en caso de serlo, False caso contrario
+'''
+    return cadena_numeros.isdigit()
+ 
+#6.3
+
+def stark_menu_principal(max_intentos=3):
+    imprimir_menu()
+    contador_intentos = 0
+    opcion = input("Ingrese el número de una opción: ")
+    
+
+    while contador_intentos < max_intentos:
+        if validar_entero(opcion) and int(opcion) in range(1, 5):
+            return int(opcion)
+        else:
+            contador_intentos += 1
+            
+            if contador_intentos == max_intentos:
+                return -1
+            else:
+                print("Opción inválida. Intento {0}/{1}".format(contador_intentos, max_intentos))
+                imprimir_menu()
+                opcion = input("Ingrese el número de una opción: ")
+
+#7
+
+def stark_marvel_app (lista_personajes):
+    
+    '''Crear la función 'stark_marvel_app' la cual recibirá por parámetro 
+    la lista de héroes y se encargará de la ejecución principal de nuestro programa. 
+     Debe informar por consola en caso de seleccionar una 
+    opción incorrecta y volver a pedir el dato al usuario. Reutilizar
+    las funciones con prefijo 'stark_' donde crea correspondiente.'''
+    
+    stark_normalizar_datos(lista_personajes)
+    
+    while True:
+        opcion = stark_menu_principal()
+        
+        match opcion:
+        
+            case 1:
+                stark_imprimir_nombres_heroes(lista_personajes)
+            case 2:
+                stark_imprimir_nombres_alturas(lista_personajes)
+            case 3:
+                stark_calcular_imprimir_heroe(lista_personajes, "maximo", "fuerza") 
+            case 4:
+                stark_calcular_imprimir_promedio_altura(lista_personajes)
+            case -1:
+                print("Salio")
+                break
+            
+        clear_console()
+
 
 #main 
 
-stark_normalizar_datos(lista_personajes)
-
-
-
-
-
-stark_imprimir_nombres_heroes(lista_personajes)
-
-stark_imprimir_nombres_alturas(lista_personajes)
+stark_marvel_app(lista_personajes)
+   
--- Progra_labo _I/string/Strings.py
+++ Progra_labo _I/string/Strings.py
@@ -5,6 +5,16 @@
 # string todo en minúsculas.
 
 
+import os
+def clear_console() -> None:
+    """
+    It waits for the user to hit enter 
+    to clear the console and redisplay the appropriate thing.
+    """
+    _ = input('Press a key to continue...')
+    os.system('cls')
+
+
 def muestra_menu():
     print("ejercicio: 1")
     print("ejercicio: 2")
@@ -103,6 +113,9 @@ def unir_nombres(lista_nombres: list) :
     texto_nombres = '\n'. join(lista_nombres)
     
     return texto_nombres
+
+
+
 def crear_mail(nombre:str, apelido:str):
 
     nombre_sin_espacios = elimina_espacios( nombre)
@@ -111,11 +124,112 @@ def crear_mail(nombre:str, apelido:str):
     nombre_minuscula = cambiar_string_minuscula(nombre_sin_espacios)
     apelido_minuscula =cambiar_string_minuscula(apellido_sin_espacio)
     
-    email_crado = f"{nombre_minuscula[0]}.{apelido_minuscula}@utn-fra.com.ar" 
+    email_crado = f"{nombre_minuscula[0]}_{nombre_minuscula}.{apelido_minuscula}@utn-fra.com.ar" 
    
     return email_crado
 
+
+
+def concatenar_palabras(lista_palabras):
+    if len(lista_palabras) == 0:
+        return ""
+    elif len(lista_palabras) == 1:
+        return lista_palabras[0]
+    else: 
+        # une todas las palabras con (,) excepto la última
+        palabras_concantenada = ', '.join(lista_palabras[:-1]) 
+        # agrega la última palabra a la cadena
+        palabras_concantenada += ' y ' + lista_palabras[-1]  
+        
+        return palabras_concantenada
     
+
+
+def verifica_tarjeta(numero_tarjeta):
+    # Verificar si el número de tarjeta contiene solo caracteres numéricos
+    if not numero_tarjeta.isnumeric():
+        return "Número de tarjeta inválido"
+    
+    # Obtener los últimos cuatro dígitos
+    ultimos_cuatro_digitos = numero_tarjeta[12:]
+    
+    # Obtener la longitud de la cadena de número de tarjeta
+    longitud = len(numero_tarjeta)
+    
+    # Obtener el número de caracteres que se deben ocultar
+    num_caracteres_ocultos = 12
+    
+    # Crear una cadena de asteriscos de la misma longitud que el número de caracteres que se deben ocultar
+    asteriscos = '*' * num_caracteres_ocultos
+    
+    # Combinar los asteriscos y los últimos cuatro dígitos para formar la cadena final
+ 
+    resultado = "{0} {1}".format(asteriscos,ultimos_cuatro_digitos)
+    
+    return resultado
+def rellenar_bits(numero_binario: str) -> str :
+    
+    numero = numero_binario.zfill(8)
+    return numero
+
+def filtra_usuario(correo):
+     filtro = correo.split("@")
+     
+     usuario = filtro[0]
+     
+     return usuario
+ 
+def filtrar_domino(dominio):
+    filtrado = dominio.split(".")
+    dominio_nombre = filtrado[1]
+    return dominio_nombre
+
+def elimina_numero_simbolos(texto):
+    texto_limpio = ""
+  
+    for caracter in texto:
+        if caracter.isalpha() or caracter == " ":
+            texto_limpio += caracter
+    
+    return texto_limpio
+
+def convierte_acronimo(texto):
+    acronimo = ""
+    palabras = texto.split()
+    
+    for palabra in palabras:
+        acronimo += palabra[0]
+        
+    acronimo_mayusucla = acronimo.upper()
+    return acronimo_mayusucla
+def reemplaza_mayuscula_minuscula(cadena):
+    cadena_convertida = ""
+    for caracter in cadena:
+        
+        if caracter.isupper():
+            cadena_convertida += caracter.lower()
+        elif caracter.islower():
+            cadena_convertida += caracter.upper()
+    return cadena_convertida
+
+def calcula_cantida_numeros_cadena(cadena_caracteres):
+    contador_numeros = 0
+    for caracter in cadena_caracteres:
+        if caracter.isnumeric():
+            contador_numeros += 1
+    return contador_numeros
+def unir_correos(lista_correos):
+    correos_unidos = ";".join(lista_correos)
+    return correos_unidos
+
+def contar_palabras(texto):
+    palabras = texto.split()
+    diccionario = {}
+    for palabra in palabras:
+        diccionario[palabra] = texto.count(palabra)
+    return diccionario
+
+
 while True:
     muestra_menu()
     opcion_str = input("Ingrese una opcion: ")
@@ -203,28 +317,111 @@ while True:
             print(mail)
           
         case 11:
-          pass
+            #Escribir una función que tome una lista de palabras
+            # y devuelva un string que contenga todas las palabras 
+            # concatenadas con comas y "y" antes de la última palabra. 
+            # Por ejemplo, si la lista es ["manzanas", "naranjas", "bananas"], 
+            # el string resultante debería ser "manzanas, naranjas y bananas"..
+            lista = ["manzanas", "naranjas", "bananas"]
+            resultado = concatenar_palabras(lista)
+            print(resultado) # Salida: "manzanas, naranjas y bananas"
+                   
         case 12:
+            #Escribir una función que tome un número de tarjeta de crédito
+            # como input, verificar que todos los caracteres sean
+            # numéricos y devolver los últimos cuatro dígitos y los 
+            # primeros dígitos como asteriscos,
+            # por ejemplo: "**** **** **** 1234". 
+            
+            numero_tarjeta = input("Ingrese el  número de la tarjeta de crédito: ")
+            
+            numero_tarjeta_verificada = verifica_tarjeta(numero_tarjeta)
+            print(numero_tarjeta_verificada)
             pass
         case 13:
-            pass
+            #Escribir una función que tome un correo electrónico y elimine 
+            # cualquier carácter después del símbolo @, por 
+            # ejemplo: "usuario@gmail.com" -> "usuario
+            correo = "usuario@gmail.com"
+            usuario = filtra_usuario(correo)
+            print(usuario)
+            
+    
+
         case 14:
-          pass
+            #Escribir una función que tome una URL y devuelva solo el nombre 
+            # de dominio, por ejemplo: "https://www.ejemplo.com.ar/pagina1"
+            # -> "ejemplo"..
+            dominio = "https://www.ejemplo.com.ar/pagina1"
+            
+            dominio_nombre = filtrar_domino(dominio)
+            
+            print(dominio_nombre)
+    
         case 15:
-            break
+            #Escribir una función que tome una cadena 
+            # de texto y devuelva solo los caracteres alfabéticos, 
+            # eliminando cualquier número o símbolo 
+            # (sólo son válidos letras y espacios),
+            # por ejemplo: "H0l4, m4nd0!" -> "Hl mnd”
+            texto ="H0l4, m4nd0!"
+            
+            texto_limpio = elimina_numero_simbolos(texto)
+            print(texto_limpio)
+         
         case 16:
-           pass
+            #Escribir una función que tome una cadena de texto
+            # y la convierta en un acrónimo, tomando la primera
+            # letra de cada palabra, por ejemplo: "Universidad 
+            # Tecnológica Nacional Facultad Regional Avellaneda"
+            # -> "UTNFRA”.
+            acronimo = convierte_acronimo("Universidad Tecnológica Nacional Facultad Regional Avellaneda")
+            print(acronimo)
+    
         case 17:
-          pass
+            #Escribir una función que tome un número binario y lo convierta
+            #en una cadena de 8 bits, rellenando con ceros a la izquierda 
+            # si es necesario, por ejemplo: "101" -> "00000101".
+            numeros_binarios = input("Ingres numero binarios: ")
+            numeros_binarios_bits = rellenar_bits(numeros_binarios)
+            print(numeros_binarios_bits)
         case 18:
+            #Escribir una función que tome una cadena de caracteres y 
+            # reemplace todas las letras mayúsculas por letras minúsculas 
+            # y todas las letras minúsculas por letras mayúsculas,
+            # por ejemplo: "HoLa" -> "hOlA"
+            cadena = "HoLa"
+            cadena_covertida = reemplaza_mayuscula_minuscula(cadena)
+            print(cadena_covertida)
             pass
         case 19:
-            pass
+            #scribir una función que tome una cadena de caracteres y cuente
+            # la cantidad de dígitos que contiene, por ejemplo: 
+            # "1234 Hola Mundo" -> contiene 4 dígitos.
+            cadena_caracteres = "1278934 Hola Mundo"
+            caracteres_contados = calcula_cantida_numeros_cadena(cadena_caracteres)
+            print("contiene {0} dígitos".format(caracteres_contados))
         case 20:
-          pass
+          #Escribir una función que tome una lista de direcciones de correo 
+          # electrónico y las una en una sola cadena separada por punto 
+          # y coma, 
+          # por ejemplo: ["juan@gmail.com", "maria@hotmail.com"]
+          # -> "juan@gmail.com;maria@hotmail.com".
+          lista_correos =["juan@gmail.com", "maria@hotmail.com"]
+          coreos_unidos = unir_correos(lista_correos)
+          print(coreos_unidos)
         case 21:
-            break
+            #Crear una función que reciba como parámetro un string y devuelva un diccionario 
+            # donde cada clave es una palabra y cada valor es un entero que indica cuántas 
+            # veces aparece esa palabra dentro del string.
+            texto = "Este es un ejemplo de texto. Este texto tiene varias palabras, algunas de ellas se repiten."
+            cantidad_aparece = contar_palabras(texto)
+            print(cantidad_aparece)
+
         case 22:
+
+            
             break
         case _:
                 print('Error no disponible.')
+    clear_console()
